4eec87954d01269e8c63badf038054d5aa4cc56e
Paulin Bekambo <56966573+pbekambo@users.noreply.github.com>
2023-07-18 12:07:39 +0200

    Create wish_card.py

---
+++ wish_card.py
@@ -0,0 +1,33 @@
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from QtGui import QPainter, QBrush, QPen
+from PyQt5.QtCore import Qt
+
+import sys
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = "PyQt5 Drawing Tutorial"
+        self.top= 150
+        self.left= 150
+        self.width = 500
+        self.height = 500
+        self.InitWindow()
+
+  def InitWindow(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()     
+      
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.black,  5, Qt.SolidLine))
+        painter.drawRect(40, 40, 400, 200)
+  
+if __name__ == "__main__":
+  App = QApplication(sys.argv)
+  window = Window()
+  sys.exit(App.exec())
+  
+  
